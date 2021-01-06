26b0f3a49714f7ba6eeea288e4a857fb7dc4a1f2
Asad Siddiqui <38368758+asadsid95@users.noreply.github.com>
2021-01-05 20:04:31 -0500

    Added IF EXISTS to drop table
    Saw it on Q&A that without it, program kept crashing

--- sql_queries.py
+++ sql_queries.py
@@ -1,10 +1,10 @@
-# DROP TABLES
+v# DROP TABLES
 
-songplay_table_drop = "DROP TABLE songplay"
-user_table_drop = "DROP TABLE user"
-song_table_drop = "DROP TABLE song"
-artist_table_drop = "DROP TABLE artist"
-time_table_drop = "DROP TABLE time"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+user_table_drop = "DROP TABLE IF EXISTS user"
+song_table_drop = "DROP TABLE IF EXITS song"
+artist_table_drop = "DROP TABLE IF EXISTS artist"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
